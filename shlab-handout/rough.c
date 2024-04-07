591bc18a014ff52d64e466f584a997c7f0e57da5
LVL_UP <126270067+udithdoddapaneni@users.noreply.github.com>
2024-04-07 16:17:21 +0530

    Add files via upload

---
+++ shlab-handout/rough.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+
+
+int main(){
+
+    while (1)
+    {
+        /* code */
+    }
+    
+    
+}
